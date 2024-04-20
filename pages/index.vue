77ddb85abeb95d344659f6525e3b524626a425fd
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-04-20 18:36:23 -0300

    [Fix] formatando hora e data em pt

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,42 @@
   <div class="flex pt-[40px] flex-col items-center">
     <h1 class="text-7xl text-green-500">Bem vindo!</h1>
     <div class="flex flex-col items-center text-3xl">
-      <p>Hora atual: {{ new Date().toLocaleTimeString() }}</p>
+      <p>Hora atual: {{ formattedTime }}</p> 
       <p>
-        Data de hoje: {{ new Date().toLocaleDateString() }}
+        Data de hoje: {{ formattedDate }}
       </p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    formatDate(date) {
+      const options = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      };
+      return date.toLocaleDateString('pt-BR', options);
+    },
+    formatTime(time) {
+      const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+      };
+      return time.toLocaleTimeString('pt-BR', options);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.formatDate(new Date());
+    },
+    formattedTime() { 
+      return this.formatTime(new Date());
+    }
+  }
+}
+</script>
