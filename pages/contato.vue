6fc4d8fc0ca3a0db0c2ec20858397d492d72f42c
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-04-20 06:26:57 -0300

    [Fix] ancora insta

--- pages/contato.vue
+++ pages/contato.vue
@@ -4,7 +4,7 @@
         <div class="flex space-x-4">
             <a href="https://github.com/brunohoinacki" target="_blank">Github</a>
             <a href="https://www.linkedin.com/in/brunohoinacki/" target="_blank">Linkedin</a>
-            <a href="https://www.instagram.com/brunohoinacki/" target="_blank">Instagram</a>
+            <a href="https://www.instagram.com/this.bruno_hoinacki/" target="_blank">Instagram</a>
             <a href="mailto:brunohoinackib@gmail.com">E-mail: brunohoinackib@gmail</a>
             <a href="https://wa.me/5551980456224?text=Fala+Brun%C3%A3o%21%21%21%21+Vim+pelo+site.">(51) 9 8045-6224 (WhatsApp)</a>
         </div>
