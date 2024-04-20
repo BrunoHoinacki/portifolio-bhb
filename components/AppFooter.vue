c2cbbb091de620b7c9d7854f8deff194cca7ee2c
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-04-20 05:03:50 -0300

    [Feature] ColorMode

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="fixed bottom-0 left-0 w-full bg-pink-500 text-white py-4 text-center">
+    <footer class="fixed bottom-0 left-0 w-full bg-green-500 text-white py-4 text-center">
         <div class="container mx-auto">
             <span>
                 Sou um footer
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/color-mode'],
   app: {
     head: {
       charset: 'utf-8',
