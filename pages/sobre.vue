755d464817b8b7d2f3a078be5cb71b2eb04d44ba
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-04-21 17:07:24 -0300

    [Test] testando auto-assist vercel

--- pages/sobre.vue
+++ pages/sobre.vue
@@ -2,5 +2,6 @@
   <div class="flex flex-col items-center text-center md:pt-40 p-16">
     <p class="text-4xl md:text-7xl p-3">Prazer me chamo Bruno Hoinacki</p>
     <p class="text-4xl md:text-7xl p-3">Sou desenvolvedor FullStack</p>
+    <p class="text-4xl md:text-7xl p-3">Cursando analise e desenvolvimento de sistemas</p>
   </div>
 </template>
