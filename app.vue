e0a11f23d1d596ec7fb91a865e7553a12248ee4f
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-06-29 01:34:21 -0300

    ajustando

--- app.vue
+++ app.vue
@@ -6,19 +6,31 @@
   </div>
 </template>
 
-<script setup>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
 
-useHead({
-  htmlAttrs: {
-    lang: 'pt-br'
-  },
-  title: 'LevelUp - Criação de Sites e Plataformas Web',
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { name: 'description', content: 'Conheça o portfólio de Bruno Hoinacki, desenvolvedor fullstack' },
-    { name: 'author', content: 'Bruno Hoinacki' },
-    { name: 'robots', content: 'index, follow' }
-  ],
-});
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Joga-Aqui.com - Os Melhores Jogos Online para Crianças',
+  ogTitle: 'Joga-Aqui.com - Jogos Divertidos e Educativos para Crianças',
+  description: 'Descubra os melhores jogos online gratuitos para crianças no Joga-Aqui.com! Diversão garantida com jogos educativos, quebra-cabeças, aventuras e muito mais.',
+  ogDescription: 'No Joga-Aqui.com, você encontra uma variedade de jogos online gratuitos e seguros para crianças. Venha se divertir com jogos educativos, quebra-cabeças, aventuras e muito mais.',
+  ogImage: '/images/logo-fundo-azul.png',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Joga-Aqui.com - Jogos Online para Crianças',
+  twitterDescription: 'Jogue os melhores jogos online gratuitos para crianças no Joga-Aqui.com. Diversão e aprendizado garantidos!',
+  twitterImage: '/images/logo-fundo-azul.png',
+  keywords: 'jogos online, jogos para crianças, jogos educativos, jogos gratuitos, diversão infantil, jogos seguros',
+  robots: 'index, follow',
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
-  tailwindcss: {
-    cssPath: "~/assets/css/tailwind.css",
-    configPath: "tailwind.config.js",
-  },
   devtools: { enabled: true },
-});
+  modules: ["@nuxt/ui"],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+        }
+      ]
+    }
+  }
+})
