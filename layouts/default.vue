1856378b094afbc87994bab385bd192389bd6130
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-06-29 20:09:07 -0300

    [ref] fechando modal no routeamento

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,16 +20,27 @@
 <script>
 import MenuLateral from '~/components/MenuLateral.vue';
 import AppHeader from '~/components/AppHeader.vue';
+import AppFooter from '~/components/AppFooter.vue'; // Adicione esta linha se você tiver o componente AppFooter
 
 export default {
   components: {
     MenuLateral,
     AppHeader,
+    AppFooter, // Adicione esta linha se você tiver o componente AppFooter
   },
   data() {
     return {
       showMenu: false
     };
+  },
+  mounted() {
+    this.$router.beforeEach((to, from, next) => {
+      this.showMenu = false;
+      next();
+    });
+  },
+  beforeDestroy() {
+    this.$router.beforeEach(() => { }); // Remove o ouvinte de eventos de roteamento
   }
 };
 </script>
